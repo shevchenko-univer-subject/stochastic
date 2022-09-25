df71c997bb386157304b0a3d8d5eb0c64b76b207
uzurpastor <shevchenko.andrey.uzurpastor@gmail.com>
2022-09-25 12:54:45 +0300

    devel: add values inputs for terminal

--- lr1/lib/wander.rb
+++ lr1/lib/wander.rb
@@ -1,14 +1,32 @@
 require_relative 'wander/experiment'
 require_relative 'wander/statistic'
 
+puts 'Hi. Lets model a point random wandering'
+puts "Set space size"
+
+print 'x = '
+x = gets.chomp.to_i
+
+print 'y = '
+y = gets.chomp.to_i
+
+print 'Monte-Carlo sample size is '
+sample_size = gets.chomp.to_i
+
 space_size = {
-  x: 10, 
-  y: 10
+  x: x, 
+  y: y
 }
 
-sample_size = 10000
-
+puts 'Running...'
 experiment = Wander::Experiment.call(space_size: space_size, sample_size: sample_size)
+
+puts 'Process...'
 statistic  = Wander::Statistic.new(experiment.statistic_data)
 
-p statistic.data
+
+puts " q for north: #{statistic.data[:north]}"
+puts " q for south: #{statistic.data[:south]}"
+puts " q for east:  #{statistic.data[:east]}"
+puts " q for west:  #{statistic.data[:west]}"
+puts " q stopped:   #{statistic.data[:stoped]}"
