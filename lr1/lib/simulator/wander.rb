92806a22f46fa2d7c522ca7f9efdd46a7a4ffc7a
uzurpastor <shevchenko.andrey.uzurpastor@gmail.com>
2022-09-25 13:22:30 +0300

    create a new module for calculating probability

--- lr1/lib/simulator/wander.rb
+++ lr1/lib/simulator/wander.rb
@@ -1,34 +1,36 @@
-require_relative 'wander/experiment'
-require_relative 'wander/statistic'
+module Simulator
+  require_relative 'wander/experiment'
+  require_relative 'wander/statistic'
 
-puts 'Hi. Lets model a point random wandering'
-puts "Set space size"
+  puts 'Hi. Lets model a point random wandering'
+  puts "Set space size"
 
-print 'x = '
-x = gets.chomp.to_i
+  print 'x = '
+  x = gets.chomp.to_i
 
-print 'y = '
-y = gets.chomp.to_i
+  print 'y = '
+  y = gets.chomp.to_i
 
-print 'Monte-Carlo sample size is '
-sample_size = gets.chomp.to_i
+  print 'Monte-Carlo sample size is '
+  sample_size = gets.chomp.to_i
 
-space_size = {
-  x: x, 
-  y: y
-}
+  space_size = {
+    x: x, 
+    y: y
+  }
 
-puts 'Running...'
-experiment = Wander::Experiment.call(space_size: space_size, sample_size: sample_size)
+  puts 'Running...'
+  experiment = Wander::Experiment.call(space_size: space_size, sample_size: sample_size)
 
-puts 'Process...'
-statistic  = Wander::Statistic.new(experiment.statistic_data)
+  puts 'Process...'
+  statistic  = Wander::Statistic.new(experiment.statistic_data)
 
-print "Start position was "
-puts "[x = #{statistic.meta_data[:start_position][:x]}, y = #{statistic.meta_data[:start_position][:y]}]"
+  print "Start position was "
+  puts "[x = #{statistic.meta_data[:start_position][:x]}, y = #{statistic.meta_data[:start_position][:y]}]"
 
-puts "q for north: #{statistic.data[:north]}"
-puts "q for south: #{statistic.data[:south]}"
-puts "q for east:  #{statistic.data[:east]}"
-puts "q for west:  #{statistic.data[:west]}"
-puts "q stopped:   #{statistic.data[:stoped]}"
+  puts "q for north: #{statistic.data[:north]}"
+  puts "q for south: #{statistic.data[:south]}"
+  puts "q for east:  #{statistic.data[:east]}"
+  puts "q for west:  #{statistic.data[:west]}"
+  puts "q stopped:   #{statistic.data[:stoped]}"
+end
