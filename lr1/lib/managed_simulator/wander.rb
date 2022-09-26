bce75726f83e866261b48c26c6fc2a55bfc312c6
uzurpastor <shevchenko.andrey.uzurpastor@gmail.com>
2022-09-26 17:11:05 +0300

    wrap scripts in methods

--- lr1/lib/managed_simulator/wander.rb
+++ lr1/lib/managed_simulator/wander.rb
@@ -2,64 +2,66 @@ module ManagedSimulator
   require_relative 'wander/experiment'
   require_relative 'wander/statistic'
 
-  puts 'Hi. Lets model a point random wandering'
+  def self.run  
+    puts 'Hi. Lets model a point random wandering'
 
-  print 'Do you want set custom probability values?(y,n) '
+    print 'Do you want set custom probability values?(y,n) '
 
-  data = if gets.chomp == 'y'
-    empty_data = {
-      meta: {
-        sample_size: nil
-      },
-      probability: {
-        north:  nil,
-        south:  nil,
-        east:   nil,
-        west:   nil,
-        stopped: nil
+    data = if STDIN.gets.chomp == 'y'
+      empty_data = {
+        meta: {
+          sample_size: nil
+        },
+        probability: {
+          north:  nil,
+          south:  nil,
+          east:   nil,
+          west:   nil,
+          stopped: nil
+        }
       }
-    }
 
-    print 'Monte-Carlo sample size is '
-    empty_data[:meta].merge!(sample_size: gets.chomp.to_i)
+      print 'Monte-Carlo sample size is '
+      empty_data[:meta].merge!(sample_size: STDIN.gets.chomp.to_i)
 
-    %i[north south east west stopped].each do |el|
-      print "probability #{el}\t= "
-      value = gets.chomp.to_f
-      empty_data[:probability].merge!({"#{el}": value})
+      %i[north south east west stopped].each do |el|
+        print "probability #{el}\t= "
+        value = STDIN.gets.chomp.to_f
+        empty_data[:probability].merge!({"#{el}": value})
+      end
+      empty_data
+    else
+      {
+        meta: {
+          sample_size: 10000
+        },
+        probability: {
+          north:  0.25,
+          south:  0.25,
+          east:   0.25,
+          west:   0.25,
+          stopped: 0
+        }
+      }
     end
-    empty_data
-  else
-    {
-      meta: {
-        sample_size: 10000
-      },
-      probability: {
-        north:  0.25,
-        south:  0.25,
-        east:   0.25,
-        west:   0.25,
-        stopped: 0
-      }
-    }
-  end
 
-  puts "\n"
-  puts "Monte-Carlo sample size is #{data[:meta][:sample_size]}"
-  puts "Probability values is #{data[:probability].inspect}"
+    puts "\n"
+    puts "Monte-Carlo sample size is #{data[:meta][:sample_size]}"
+    puts "Probability values is #{data[:probability].inspect}"
 
-  puts "\n"
-  puts 'Running...' 
-  experiment = Wander::Experiment.call(data)
+    puts "\n"
+    puts 'Running...' 
+    experiment = Wander::Experiment.call(data)
 
-  puts 'Process...'
-  statistic = Wander::Statistic.call(experiment.collected_data)
-  puts "\n"
+    puts 'Process...'
+    statistic = Wander::Statistic.call(experiment.collected_data)
+    puts "\n"
 
-  puts 'Info: '
-  statistic.data.keys.each do |el|
-    puts "\t #{el.capitalize}: "
-    puts "\t\t Exit probability #{statistic.data[el][:exit_prob]}"
-    puts "\t\t Uncertaity #{statistic.data[el][:uncertainty]}"
+    puts 'Info: '
+    statistic.data.keys.each do |el|
+      puts "\t #{el.capitalize}: "
+      puts "\t\t Exit probability #{statistic.data[el][:exit_prob]}"
+      puts "\t\t Uncertaity #{statistic.data[el][:uncertainty]}"
+    end
   end
 end
--- lr1/lib/unmanaged_simulator/wander.rb
+++ lr1/lib/unmanaged_simulator/wander.rb
@@ -2,35 +2,37 @@ module UnmanagedSimulator
   require_relative 'wander/experiment'
   require_relative 'wander/statistic'
 
-  puts 'Hi. Lets model a point random wandering'
-  puts "Set space size"
+  def self.run
+    puts 'Hi. Lets model a point random wandering'
+    puts "Set space size"
 
-  print 'x = '
-  x = gets.chomp.to_i
+    print 'x = '
+    x = STDIN.gets.chomp.to_i
 
-  print 'y = '
-  y = gets.chomp.to_i
+    print 'y = '
+    y = STDIN.gets.chomp.to_i
 
-  print 'Monte-Carlo sample size is '
-  sample_size = gets.chomp.to_i
+    print 'Monte-Carlo sample size is '
+    sample_size = STDIN.gets.chomp.to_i
 
-  space_size = {
-    x: x, 
-    y: y
-  }
+    space_size = {
+      x: x, 
+      y: y
+    }
 
-  puts 'Running...'
-  experiment = Wander::Experiment.call(space_size: space_size, sample_size: sample_size)
+    puts 'Running...'
+    experiment = Wander::Experiment.call(space_size: space_size, sample_size: sample_size)
 
-  puts 'Process...'
-  statistic  = Wander::Statistic.new(experiment.statistic_data)
+    puts 'Process...'
+    statistic  = Wander::Statistic.new(experiment.statistic_data)
 
-  print "Start position was "
-  puts "[x = #{statistic.meta[:start_position][:x]}, y = #{statistic.meta[:start_position][:y]}]"
+    print "Start position was "
+    puts "[x = #{statistic.meta[:start_position][:x]}, y = #{statistic.meta[:start_position][:y]}]"
 
-  puts "q for north: #{statistic.data[:north]}"
-  puts "q for south: #{statistic.data[:south]}"
-  puts "q for east:  #{statistic.data[:east]}"
-  puts "q for west:  #{statistic.data[:west]}"
-  puts "q stopped:   #{statistic.data[:stopped]}"
+    puts "q for north: #{statistic.data[:north]}"
+    puts "q for south: #{statistic.data[:south]}"
+    puts "q for east:  #{statistic.data[:east]}"
+    puts "q for west:  #{statistic.data[:west]}"
+    puts "q stopped:   #{statistic.data[:stopped]}"
+  end
 end
