79f0ab766a70ef97c5cdf1ba421b1b44edcb0d8c
uzurpastor <shevchenko.andrey.uzurpastor@gmail.com>
2022-09-26 11:55:41 +0300

    realize experiment class for calculator module

--- lr1/lib/calculator/wander.rb
+++ lr1/lib/calculator/wander.rb
@@ -1,1 +1,20 @@
-wander.rb
+module Calculator
+  require_relative 'wander/experiment'
+  require_relative 'wander/statistic'
+
+  data = {
+    meta: {
+      sample_size: 10000
+    },
+    probability: {
+      north:  0.25,
+      south:  0.25,
+      east:   0.25,
+      west:   0.25,
+      stoped: 0
+    }
+  }
+
+  experiment = Wander::Experiment.call(data)
+  p experiment.collected_data
+end
---
+++ lr1/lib/calculator/wander/experiment.rb
@@ -0,0 +1,61 @@
+module Calculator
+  module Wander
+    class Experiment
+      def self.call(*args)
+        new(*args).call
+      end
+
+      def initialize(data)
+        @meta = data[:meta]
+        @probability = data[:probability]
+        @result = {
+          stoped: 0,
+          north: 0,
+          south: 0,
+          east: 0,
+          west: 0
+        }
+      end
+
+      def call
+        validate_probability_data
+
+        sample_size.times do 
+          record_result(random_situation)
+        end
+        return self
+      end
+
+      def collected_data
+        {
+          meta: @meta,
+          probability: @probability,
+          result: @result
+        }
+      end
+      
+      private
+        def validate_probability_data
+          raise 'Sum of probability should to be equal 1' unless @probability.values.sum == 1
+        end
+
+        def random_situation
+          rand_v = Random.rand
+          from = 0; to = 0
+
+          @probability.each do |situation, prob|
+            from = to; to += prob
+            return situation if rand_v.between?(from, to)
+          end
+        end
+
+        def record_result situation
+          @result[situation] += 1
+        end
+
+        def sample_size
+          @meta[:sample_size]
+        end
+    end
+  end
+end
