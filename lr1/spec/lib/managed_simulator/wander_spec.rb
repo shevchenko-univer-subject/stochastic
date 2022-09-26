f57c87c9780e959b292925870fa872786eeecb05
uzurpastor <shevchenko.andrey.uzurpastor@gmail.com>
2022-09-26 17:11:35 +0300

    add to project rspec gem

---
+++ lr1/spec/lib/managed_simulator/wander_spec.rb
@@ -0,0 +1,23 @@
+RSpec.describe ManagedSimulator::Wander do 
+  describe 'symmetry  without stopping' do
+    describe 'without stopping' do 
+      example 'has small sample size' do 
+      end
+      example 'has large sample size' do 
+      end
+    end
+    describe 'with stopping' do 
+      example 'which weak' do 
+      end
+      example 'which hard' do 
+      end
+    end
+  end
+
+  describe 'one-dimensional horizontal wandering' do 
+    example 'has symmetry position' do 
+    end
+    example 'has asymmetry position' do 
+    end
+  end
+end
---
+++ lr1/spec/spec_helper.rb
@@ -0,0 +1,2 @@
+require_relative '../lib/managed_simulator/wander'
+include ManagedSimulator
