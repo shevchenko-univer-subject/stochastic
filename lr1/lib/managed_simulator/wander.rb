80c1a4acd9a224450d718487922b57b026dcd884
uzurpastor <shevchenko.andrey.uzurpastor@gmail.com>
2022-09-29 11:08:12 +0300

    unload module run method

--- lr1/lib/managed_simulator/wander.rb
+++ lr1/lib/managed_simulator/wander.rb
@@ -2,49 +2,58 @@ module ManagedSimulator
   require_relative 'wander/experiment'
   require_relative 'wander/statistic'
 
-  def self.run  
+  def run  
     puts 'Hi. Lets model a point random wandering'
 
     print 'Do you want set custom probability values?(y,n) '
 
     data = if STDIN.gets.chomp == 'y'
-      empty_data = {
-        meta: {
-          sample_size: nil,
-          space_size: nil
-        },
-        probability: {
-          north:  nil,
-          south:  nil,
-          east:   nil,
-          west:   nil,
-          stopped: nil
-        }
-      }
-
-      print 'Monte-Carlo sample size is '
-      empty_data[:meta].merge!(sample_size: STDIN.gets.chomp.to_i)
-
-      puts "Space size is"
-      print "\tx = "
-      x = STDIN.gets.chomp.to_i
-      print "\ty = "
-      y = STDIN.gets.chomp.to_i
-      empty_data[:meta].merge!(space_size: {x: x, y: y})
-
-      %i[north south east west stopped].each do |el|
-        print "probability #{el}\t= "
-        value = STDIN.gets.chomp.to_f
-        empty_data[:probability].merge!({"#{el}": value})
-      end
-      empty_data
+      get_data_from_term_form    
     else
+      default_data
+    end
+
+    print_data
+
+    puts "\n"
+    puts 'Running...' 
+    experiment = Wander::Experiment.call(data)
+
+    puts 'Process...'
+    statistic = Wander::Statistic.call(experiment.collected_data)
+    puts "\n"
+
+    print_statistic_info(statistic)
+  end
+  
+  private
+    def print_statistic_info(statistic)
+      statistic.data.keys.each do |el|
+        puts 'Info: '
+          puts "\t #{el.capitalize}: "
+          puts "\t\t Exit probability #{statistic.data[el][:exit_prob]}"
+          puts "\t\t Uncertaity #{statistic.data[el][:uncertainty]}"
+      end
+    end
+
+    def print_data
+      puts "\n"
+      puts "Monte-Carlo sample size is #{data[:meta][:sample_size]}"
+      puts "Probability values is #{data[:probability].inspect}"
+      puts "Start position is #{data[:meta][:start_position]}"
+    end
+
+    def default_data
       {
         meta: {
           sample_size: 10000,
           space_size: {
             x: 10,
             y: 10
+          },
+          start_position: {
+            x: 5,
+            y: 5
           }
         },
         probability: {
@@ -57,23 +66,44 @@ module ManagedSimulator
       }
     end
 
-    puts "\n"
-    puts "Monte-Carlo sample size is #{data[:meta][:sample_size]}"
-    puts "Probability values is #{data[:probability].inspect}"
+    def get_data_from_term_form
+      empty_data = {
+        meta: {
+          sample_size: nil,
+          space_size: nil,
+          start_position: nil
+        },
+        probability: {
+          north:  nil,
+          south:  nil,
+          east:   nil,
+          west:   nil,
+          stopped: nil
+        }
+      }
 
-    puts "\n"
-    puts 'Running...' 
-    experiment = Wander::Experiment.call(data)
+      print 'Monte-Carlo sample size is '
+        empty_data[:meta].merge!(sample_size: STDIN.gets.chomp.to_i)
 
-    puts 'Process...'
-    statistic = Wander::Statistic.call(experiment.collected_data)
-    puts "\n"
+      puts "Space size is"
+        print "\tx = "
+        x = STDIN.gets.chomp.to_i
+        print "\ty = "
+        y = STDIN.gets.chomp.to_i
+        empty_data[:meta].merge!(space_size: {x: x, y: y})
 
-    puts 'Info: '
-    statistic.data.keys.each do |el|
-      puts "\t #{el.capitalize}: "
-      puts "\t\t Exit probability #{statistic.data[el][:exit_prob]}"
-      puts "\t\t Uncertaity #{statistic.data[el][:uncertainty]}"
+      puts "Start position is"
+        print "\tx = "
+        x = STDIN.gets.chomp.to_i
+        print "\ty = "
+        y = STDIN.gets.chomp.to_i
+        empty_data[:meta].merge!(start_position: {x: x, y: y})
+
+      %i[north south east west stopped].each do |el|
+        print "probability #{el}\t= "
+        value = STDIN.gets.chomp.to_f
+        empty_data[:probability].merge!({"#{el}": value})
+      end
+      empty_data
     end
-  end
 end
