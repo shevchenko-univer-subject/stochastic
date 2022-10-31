c461ad7513df0877b061fe586d1997dbdd5be0b6
uzurpastor <shevchenko.andrey.uzurpastor@gmail.com>
2022-10-31 09:58:39 +0200

    fix: params in direct method

--- lr2/integral/engine/modes/direct.rb
+++ lr2/integral/engine/modes/direct.rb
@@ -10,8 +10,8 @@ module Integral
 
       def compute_function(axis) # TODO
         defirencial_functions = get_default_defirencial_functions 
-        defirencial_functions[axis].call(@borders[axis].max) - 
-          defirencial_functions[axis].call(@borders[axis].min)
+        defirencial_functions[axis].call(@borders[axis].max, @params[axis]) - 
+          defirencial_functions[axis].call(@borders[axis].min, @params[axis])
       end
 
       def compute_mistake
@@ -20,11 +20,11 @@ module Integral
 
       private
         def get_default_defirencial_functions
-          param = 1.0
+          
           {
-            x: -> (x) { -param * (x ** 2.0) * (2.0 * x - 3.0) / 6.0 },
-            y: -> (y) { -Math.exp(-param * y) / param },
-            z: -> (z) { -Math.cos(Math::PI * param * z) / (Math::PI * param) }
+            x: -> (x, param) { -param[:a] * (x ** 2.0) * (2.0 * x - 3.0) / 6.0 },
+            y: -> (y, param) { -Math.exp(-param[:m] * y) / param[:m] },
+            z: -> (z, param) { -Math.cos(Math::PI * param[:k] * z) / (Math::PI * param[:k]) }
           }
         end
 
